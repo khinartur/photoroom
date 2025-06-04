3bb763c5b3513df9e2259ee8f0c4499ce0847f7f
Arthur Khineltsev <khineltsev@gmail.com>
2025-06-05 00:26:14 +0300

    Add TS checker vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
+import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), svgr({include: '**/*.svg'})],
+    plugins: [
+        react(),
+        checker({typescript: true}),
+        tailwindcss(),
+        svgr({include: '**/*.svg'}),
+    ],
 })
